7a56397eb57d7cefef58f2fa61d5765cdd629f5d
drron <chasehult@gmail.com>
2020-02-10 18:09:36 -0800

    regex search

--- rpadutils/rpadutils.py
+++ rpadutils/rpadutils.py
@@ -7,6 +7,9 @@ import re
 import time
 import unicodedata
 import urllib
+from functools import wraps
+import errno
+import signal
 
 import aiohttp
 import backoff
@@ -725,3 +728,24 @@ def validate_json(fp):
         return True
     except:
         return False
+
+
+class TimeoutError(Exception):
+    pass
+
+
+def timeout_after(seconds=10, error_message=os.strerror(errno.ETIME)):
+    def decorator(func):
+        def _handle_timeout(signum, frame):
+            raise TimeoutError(error_message)
+
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, _handle_timeout)
+            signal.setitimer(signal.ITIMER_REAL,seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+        return wraps(func)(wrapper)
+    return decorator
