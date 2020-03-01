efe463e262bc5a6b8e3e10d38aaac714918da54d
droon <chasehult@gmail.com>
2020-02-29 19:00:46 -0800

    fix say

--- playground/playground.py
+++ playground/playground.py
@@ -15,29 +15,3 @@ class Playground(commands.Cog):
     def __init__(self, bot, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.bot = bot
-
-    @commands.command(aliases = ["parrot", "repeat"])
-    async def say(self, ctx, *, message):
-        """Make Miru parrot a phrase."""
-        message = self.emojify(message)
-        await ctx.send(message)
-
-    @commands.command()
-    async def mask(self, ctx, *, message):
-        """Sends a message as Miru."""
-        message = self.emojify(message)
-        await ctx.message.delete()
-        await ctx.send(message)
-
-    @commands.command()
-    async def yell(self, ctx, *, message):
-        """Yells some text."""
-        message = self.emojify(message)
-        await ctx.send(message.upper().rstrip(",.!?")+"!!!!!!")
-
-    def emojify(self, message):
-        emojis = list()
-        for guild in self.bot.guilds:
-            emojis.extend(guild.emojis)
-        message = rpadutils.replace_emoji_names_with_code(emojis, message)
-        return rpadutils.fix_emojis_for_server(emojis, message)
