2dde253fde5c8990cb8454a3fdf1d1b69951f1fb
droon <chasehult@gmail.com>
2020-03-04 09:42:31 -0800

    calc fix (?)

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -19,8 +19,7 @@ ALTERED_TOKENS = {'^': '**'}
 
 HELP_MSG = '''
 This calculator works by first validating the content of your query against a whitelist, and then
-executing a python eval() on it, so some common syntax wont work. Notably, you have to use
-pow(x, y) instead of x^y. Here is the full symbol whitelist:
+executing a python eval() on it, so some common syntax wont work.  Here is the full symbol whitelist:
 '''
 
 
@@ -58,18 +57,20 @@ class Calculator(commands.Cog):
         try:
             if os.name != 'nt' and sys.platform != 'win32':
                 cmd = shlex.split(cmd)
-            output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, timeout=1)
+            output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, timeout=2)
             calc_result = output.decode('utf-8').strip()
         except subprocess.TimeoutExpired:
             await ctx.send(inline('Command took too long to execute. Quit trying to break the bot.'))
             return
-
-        if len(str(calc_result)) > 0:
+        ecept
+        if len(str(calc_result)) > 1024:
+            await ctx.send(inline("The result is obnoxiously long!  Try a request under 1k characters!"))
+        elif len(str(calc_result)) > 0:
             if isinstance(calc_result, numbers.Number):
                 if calc_result > 1:
                     calc_result = round(calc_result, 3)
 
-            em = discord.Embed(color=discord.Color.blue())
+            em = discord.Embed(color=discord.Color.greyple())
             em.add_field(name='Input', value='`{}`'.format(inp))
             em.add_field(name='Result', value=calc_result)
             await ctx.send(embed=em)
